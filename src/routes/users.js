5f7f2099c3125528285b106cb19464f7b76b26bb
Nicolas Dankiewicz <nicodanki@hotmail.com>
2019-08-23 18:59:43 -0300

    SignUP added

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,4 +8,25 @@ router.get('/users/signup', (req,res)=>{
     res.render('users/signup')
 });
 
+router.post('/users/signup', (req,res)=>{
+    console.log(req.body);
+    const {name, lastname, mail, phone, password, confirm_password, healphInsurance} = req.body;
+    const errors = [];
+    const health = 'Obra Social o Prepaga';
+    if(password!= confirm_password){
+        errors.push({text: 'Las contraseñas deben ser iguales'});
+    }
+    if(password.length < 8){
+        errors.push({text: 'La contraseña debe tener mas de 8 digitos'})
+    }
+    if(healphInsurance==health){
+        errors.push({text: 'Seleccione alguna obra social'});
+    }
+    if(errors.length > 0){
+        res.render('users/signup', {errors, name, lastname, mail, phone, password, confirm_password, healphInsurance});
+    }else{
+        res.send('ok');
+    }
+});
+
 module.exports = router;
