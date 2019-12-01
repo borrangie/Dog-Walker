c162cc42b6f6c0a21d743d98cb6d9d83d588681c
nicolas <nicbritos@hotmail.com>
2019-12-01 15:08:59 -0300

    Added Google Maps API Key

--- client/ios/Runner/AppDelegate.m
+++ client/ios/Runner/AppDelegate.m
@@ -6,6 +6,8 @@
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  [GMSServices provideAPIKey:@"AIzaSyDmSV-vJ4Oh6X0gQXW2KslvR2oR48azElc"];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
