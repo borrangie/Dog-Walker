9a34a6a54af79c14cb1d779bc3f6c82a8d75720c
nicolas <nicbritos@hotmail.com>
2019-12-01 14:41:49 -0300

    Added text field types

--- client/lib/screens/components/text_field_factory.dart
+++ client/lib/screens/components/text_field_factory.dart
@@ -2,18 +2,31 @@ import 'package:flutter/material.dart';
 
 abstract class TextFieldFactory {
   static TextField generate(controller, text) {
+    return _generateTextFieldKeyboardType(controller, text, TextInputType.text);
+  }
+
+  static TextField generatePassword(controller, text) {
     return TextField(
+      obscureText: true,
       controller: controller,
       decoration: _generateDecoration(text)
     );
   }
 
-  static TextField generatePassword(controller, text) {
-    return TextField(
-        obscureText: true,
-        controller: controller,
-        decoration: _generateDecoration(text)
-    );
+  static TextField generateEmail(controller, text) {
+    return _generateTextFieldKeyboardType(controller, text, TextInputType.emailAddress);
+  }
+
+  static TextField generateNumeric(controller, text) {
+    return _generateTextFieldKeyboardType(controller, text, TextInputType.number);
+  }
+
+  static TextField generatePhone(controller, text) {
+    return _generateTextFieldKeyboardType(controller, text, TextInputType.phone);
+  }
+
+  static TextField generateDatetime(controller, text) {
+    return _generateTextFieldKeyboardType(controller, text, TextInputType.datetime);
   }
 
   static InputDecoration _generateDecoration(text) {
@@ -27,6 +40,14 @@ abstract class TextFieldFactory {
         focusedBorder: UnderlineInputBorder(
           borderSide: BorderSide(color: Colors.red),
         )
+    );
+  }
+
+  static TextField _generateTextFieldKeyboardType(controller, text, TextInputType textInputType) {
+    return TextField(
+        controller: controller,
+        keyboardType: textInputType,
+        decoration: _generateDecoration(text)
     );
   }
 }
